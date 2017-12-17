ca80eb56a7b34b649fa353f3f8c50982394e8f72
Sophie <sophie.collard@hotmail.fr>
2017-12-17 17:03:31 +0000

    Define live interpreter

--- src/main/scala/forex/services/oneforge/Interpreters.scala
+++ src/main/scala/forex/services/oneforge/Interpreters.scala
@@ -2,6 +2,13 @@ package forex.services.oneforge
 
 import java.time.OffsetDateTime
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.{ActorMaterializer, Materializer}
+import de.heikoseeberger.akkahttpcirce.ErrorAccumulatingCirceSupport
+import forex.config.OneForgeConfig
 import forex.domain._
 import monix.eval.Task
 import org.atnos.eff._
@@ -13,6 +20,15 @@ object Interpreters {
       implicit
       m1: _task[R]
   ): Algebra[Eff[R, ?]] = new Dummy[R]
+
+  def live[R](
+      oneForgeConfig: OneForgeConfig
+  )(
+      implicit
+      m1: _task[R],
+      sys: ActorSystem,
+      mat: ActorMaterializer
+  ): Algebra[Eff[R, ?]] = new Live[R](oneForgeConfig)
 }
 
 final class Dummy[R] private[oneforge] (
@@ -25,4 +41,31 @@ final class Dummy[R] private[oneforge] (
     for {
       result ← fromTask(Task.now(Rate(pair, Price(BigDecimal(100)), Timestamp.now)))
     } yield Right(result)
+}
+
+final class Live[R] private[oneforge] (
+    oneForgeConfig: OneForgeConfig
+)(
+    implicit
+    m1: _task[R],
+    sys: ActorSystem,
+    mat: Materializer
+) extends Algebra[Eff[R, ?]]
+  with ErrorAccumulatingCirceSupport {
+  override def get(
+      pair: Rate.Pair
+  ): Eff[R, Error Either Rate] = {
+    val uri = s"${oneForgeConfig.baseUri}/quotes?pairs=${pair.from}${pair.to}&api_key=${oneForgeConfig.apiKey}"
+    val request = HttpRequest(uri = uri)
+
+    implicit val ec = sys.dispatcher
+
+    for {
+      response <- fromTask(Task.fromFuture(Http().singleRequest(request)))
+      quotes <- fromTask(Task.fromFuture(Unmarshal(response.entity).to[List[Quote]]))
+    } yield quotes match {
+      case Nil    => Left(Error.ThirdParty(party = "1Forge", msg = "No quote returned"))
+      case q :: _ => Right(Rate(pair, q.price, Timestamp.now))
+    }
+  }
 }
