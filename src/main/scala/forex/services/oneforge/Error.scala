a9ffbfa1182e8b605c948c752fffbb1a4736325d
Sophie <sophie.collard@hotmail.fr>
2017-12-17 22:48:41 +0000

    Define ThirdParty error

--- src/main/scala/forex/services/oneforge/Error.scala
+++ src/main/scala/forex/services/oneforge/Error.scala
@@ -6,4 +6,5 @@ sealed trait Error extends Throwable with NoStackTrace
 object Error {
   final case object Generic extends Error
   final case class System(underlying: Throwable) extends Error
+  final case class ThirdParty(party: String, msg: String) extends Error
 }
