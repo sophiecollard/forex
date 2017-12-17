f169fda56fc32cd9138fd8afb8d88d7e3efea242
Sophie <sophie.collard@hotmail.fr>
2017-12-17 21:18:22 +0000

    Allow Processes to select between dummy and live interpreters

--- src/main/scala/forex/main/Processes.scala
+++ src/main/scala/forex/main/Processes.scala
@@ -1,16 +1,36 @@
 package forex.main
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import cats.Eval
 import forex.config._
 import forex.{ services ⇒ s }
 import forex.{ processes ⇒ p }
+import org.zalando.grafter._
 import org.zalando.grafter.macros._
 
-@readerOf[ApplicationConfig]
-case class Processes() {
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
-  implicit final lazy val _oneForge: s.OneForge[AppEffect] =
-    s.OneForge.dummy[AppStack]
+@readerOf[ApplicationConfig]
+case class Processes(
+    oneForgeConfig: OneForgeConfig
+) extends Stop {
+
+  implicit lazy val system = ActorSystem("live-interpreter")
+  implicit lazy val materializer = ActorMaterializer()
+
+  implicit final lazy val _oneForge: s.OneForge[AppEffect] = oneForgeConfig.interpreter match {
+    case "dummy" => s.OneForge.dummy[AppStack]
+    case "live"  => s.OneForge.live[AppStack](oneForgeConfig)
+    case _       => throw new RuntimeException("Invalid oneforge interpreter")
+  }
 
   final val Rates = p.Rates[AppEffect]
+
+  override def stop: Eval[StopResult] =
+    StopResult.eval("Processes") {
+      Await.result(system.terminate(), 10.seconds)
+    }
 
 }
