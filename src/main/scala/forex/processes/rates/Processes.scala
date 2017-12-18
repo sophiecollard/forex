e591a33c9bcc02fc8234b96bbcaaa170387666c4
Sophie <sophie.collard@hotmail.fr>
2017-12-18 00:59:20 +0000

    Cache rates for 5 minutes

--- src/main/scala/forex/processes/rates/Processes.scala
+++ src/main/scala/forex/processes/rates/Processes.scala
@@ -1,9 +1,15 @@
 package forex.processes.rates
 
+import java.time.OffsetDateTime
+
 import cats.Monad
-import cats.data.EitherT
+import cats.data.{EitherT, OptionT}
+import cats.implicits._
 import forex.domain._
 import forex.services._
+
+import scala.collection.mutable.Map
+import scala.concurrent.duration._
 
 object Processes {
   def apply[F[_]]: Processes[F] =
@@ -14,15 +20,34 @@ trait Processes[F[_]] {
   import messages._
   import converters._
 
+  protected val cache: Map[Rate.Pair, Rate] = Map.empty
+
   def get(
       request: GetRequest
   )(
       implicit
       M: Monad[F],
       OneForge: OneForge[F]
-  ): F[Error Either Rate] =
-    (for {
-      result ← EitherT(OneForge.get(Rate.Pair(request.from, request.to))).leftMap(toProcessError)
-    } yield result).value
+  ): F[Error Either Rate] = {
+    val pair = Rate.Pair(request.from, request.to)
+
+    val cachedResult = OptionT(cache.get(pair).pure[F])
+      .filter { rate =>
+        val secondsSinceRetrieved = OffsetDateTime.now.toEpochSecond - rate.timestamp.value.toEpochSecond
+        secondsSinceRetrieved < 5.minutes.toSeconds
+      }
+      .toRight[Error](Error.Generic)
+
+    val result = cachedResult
+      .orElse {
+        EitherT(OneForge.get(pair)).map { rate =>
+          cache += (pair -> rate)
+          rate
+        }
+      }
+      .leftMap(toProcessError)
+
+    result.value
+  }
 
 }
